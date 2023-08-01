928d7b7c2355f1a51ec764e2629b806c09d32134
Nikola <nikola.janjic88rm@gmail.com>
2023-08-01 18:52:31 +0200

    spell corrections

--- resources/js/Components/About.vue
+++ resources/js/Components/About.vue
@@ -7,7 +7,7 @@
                     <div class="flex flex-col">
                         <h2 class="text-3xl text-teal-200 lg:text-4xl font-medium lg:font-extrabold mb-3">About me</h2>
                         <p class="mb-4 mt-8 text-teal-300">I started learning PHP and SQL in 2020. I had some knowledge of HTML and CSS before that. Then I started learning Javascript for frontend part. I've never stopped learning since then, I really want to learn more and improve myself. In spring of 2022. I was at intership in company Omni Solutions, Belgrade. <br>
-                        Before going into web development, I was civil engineering student (2013-2017). I thought Math during that time (it was one of the few subjects since elementary school that I liked and was good at). <br>
+                        Before going into web development, I was civil engineering student (2013-2017). I taught Math during that time (it was one of the few subjects since elementary school that I liked and was good at). <br>
                         Generally about me, beside work and education, <br>
                         I love sports (football and basketball above all), video games, movies and TV shows, rock and (some) metal music. Also I'm an animal lover and I'm trying to help stray dogs as much as I can.
                         </p>
